5c776aab0d6a6dcb485eaa15c7476349fbf7a6fa
mateusesp <m.espositof@gmail.com>
2019-07-11 23:48:39 -0300

    instalando graphql yoga e criando o servidor com GraphQL

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
-const customExpress = require('./config/custom-express')
+const { GraphQLServer } = require('graphql-yoga')
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/database/tabelas')
 
-const app = customExpress()
-
 conexao.connect(erro => {
-  if (erro) {
-    console.log(erro)
-  }
+    if (erro) {
+        console.log(erro)
+    }
 
-  console.log('conectou no banco')
+    console.log('conectou no banco')
 
-  Tabelas.init(conexao)
+    Tabelas.init(conexao)
 })
 
-app.listen(4000, () => {
-  console.log('Servidor rodando na porta 4000')
+const resolvers = {
+    Query: {
+        status: () => "Servidor rodando"
+    }
+}
+const servidor = new GraphQLServer({
+    resolvers,
+    typeDefs: './schema.graphql'
 })
+
+servidor.start(() => console.log('servidor ouvindo'))
