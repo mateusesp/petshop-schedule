7ef81809a24ba4087e582d7f830e165720562361
mateusesp <m.espositof@gmail.com>
2019-07-15 21:53:42 -0300

    buscando um unico cliente

--- infraestrutura/crud/cliente.js
+++ infraestrutura/crud/cliente.js
@@ -10,7 +10,7 @@ class Cliente {
   buscaPorId(id) {
     const sql = `SELECT * FROM Clientes WHERE id=${id}`
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(clientes => clientes[0])
   }
 
   adiciona(item) {
